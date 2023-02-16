90d557f12321cceaa04e05f0b085ad54b3b3e32d
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-02-16 12:46:39 -0700

    Se agrega tour en español

--- es/tours.php
+++ es/tours.php
@@ -4,8 +4,8 @@
             <div class="col-lg-7">
                 <div class="section-title text-center text-lg-start">
                     <h4 class="mb-1 theme1">Excurciones y recorridos</h4>
-                    <h2 class="mb-1">Ours <span class="theme">Tours</span></h2>
-                    <p>A trip never to be forgotten</p>
+                    <h2 class="mb-1">Nuestros <span class="theme">Tours</span></h2>
+                    <p>Un viaje inolvidable</p>
                 </div>
             </div>
             <div class="col-lg-5">
@@ -36,11 +36,11 @@
                                     <div class="trend-meta bg-theme white px-3 py-2 rounded">
                                         <div class="entry-author">
                                             <i class="fa-solid fa-clock"></i>
-                                            <span class="fw-bold"> <?php echo ($tour['duracion']); ?> hours</span>
+                                            <span class="fw-bold"> <?php echo ($tour['duracion']); ?> horas</span>
                                         </div>
                                     </div>
-                                    <h5 class="theme mb-1"><i class="flaticon-location-pin"></i> <?php echo ($catSel['nombreIn']); ?></h5>
-                                    <h3 class="mb-1"><a href="/tour?id=<?php echo ($tour['id']); ?>"><?php echo ($tour['nombre_ingles']); ?></a></h3>
+                                    <h5 class="theme mb-1"><i class="flaticon-location-pin"></i> <?php echo ($catSel['nombre']); ?></h5>
+                                    <h3 class="mb-1"><a href="/tour?id=<?php echo ($tour['id']); ?>"><?php echo ($tour['nombre']); ?></a></h3>
                                     <div class="rating-main d-flex align-items-center pb-2">
                                         <div class="rating">
                                             <span class="fa fa-star checked"></span>
@@ -50,17 +50,17 @@
                                             <span class="fa fa-star checked"></span>
                                         </div>
                                     </div>
-                                    <p class=" border-b pb-2 mb-2"><?php echo substr(strip_tags($tour['descripcion_ingles']), 0, 130); ?>...</p>
+                                    <p class=" border-b pb-2 mb-2"><?php echo substr(strip_tags($tour['descripcion']), 0, 130); ?>...</p>
                                     <div class="entry-meta">
                                         <div class="row entry-author d-flex align-items-center">
                                             <div class="col-md-6">
                                                 <p class="mb-0"><span class="theme fw-bold fs-5"> $<?php echo number_format($tour['precioPromo'], 2); ?></span></span>
-                                                    <br> <span style="text-decoration:line-through;">$<?php echo number_format($tour['precio'], 2); ?></span> | Adult
+                                                    <br> <span style="text-decoration:line-through;">$<?php echo number_format($tour['precio'], 2); ?></span> | Adulto
                                                 </p>
                                             </div>
                                             <div class="col-md-6">
                                                 <p class="mb-0"><span class="theme fw-bold fs-5"> $<?php echo number_format($tour['precioNiPromo'], 2); ?></span>
-                                                    <br> <span style="text-decoration:line-through;">$<?php echo number_format($tour['precioNi'], 2); ?></span> | Kid
+                                                    <br> <span style="text-decoration:line-through;">$<?php echo number_format($tour['precioNi'], 2); ?></span> | Niño
                                                 </p>
                                             </div>
                                         </div>
